70fae20ee600698283ec1858e81bed7dd560bc96
Eduardo Ceni <eduardoceni.santos@gmail.com>
2026-08-29 10:16:11 -0300

    Add ParImpar program to determine even or odd numbers
    Implement a Java program to check if a number is even or odd.

--- atividades/exercicios02/ParImpar.java
+++ atividades/exercicios02/ParImpar.java
@@ -1,1 +1,29 @@
+/*Exercício 1: Par ou Ímpar
 
+Enunciado: Escreva um programa em Java que leia um número inteiro e informe se ele é par ou ímpar.
+
+Conceitos: Operador módulo (%) e estrutura if-else simples.
+ */
+
+package atividades.exercicios02;
+
+
+import java.util.Scanner;
+
+public class LeituraPar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Forneca um valor inteiro: ");
+        int num = sc.nextInt();
+
+        if(num%2==0)
+        {
+            System.out.println("O valor " + num + " é par");
+        }
+        else {
+            System.out.println("O valor " + num + "é impar ");
+        }
+        sc.close();
+    }
+}
